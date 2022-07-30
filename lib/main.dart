d1ba51b4f6e973d235897e40f9a4f2e4f79bd476
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-29 22:12:16 -0400

    feat: add scrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -40,86 +42,95 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Top Despesas"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          const SizedBox(
-            child: Card(
-              color: Colors.blue,
-              elevation: 5,
-              child: Text("Gráfico"),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const SizedBox(
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text("Gráfico"),
+              ),
             ),
-          ),
-          Column(
-            children: _transactions.map((transaction) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
+            Column(
+              children: _transactions.map((transaction) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
                         ),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        "R\$ ${transaction.value.toStringAsFixed(2)}",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
                         ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          transaction.title,
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "R\$ ${transaction.value.toStringAsFixed(2)}",
                           style: const TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 16,
+                            fontSize: 20,
+                            color: Colors.purple,
                           ),
                         ),
-                        Text(
-                          DateFormat('d MMM y').format(transaction.date),
-                          style: const TextStyle(
-                            color: Colors.grey,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            transaction.title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
                           ),
-                        )
-                      ],
+                          Text(
+                            DateFormat('d MMM y').format(transaction.date),
+                            style: const TextStyle(
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: "Nome da despesa",
+                      ),
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: "Valor (R\$)",
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(color: Colors.purple),
+                      ),
+                      child: const Text("Nova Transação"),
                     ),
                   ],
                 ),
-              );
-            }).toList(),
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: const [
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: "Nome da despesa",
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: "Valor (R\$)",
-                    ),
-                  ),
-                ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
