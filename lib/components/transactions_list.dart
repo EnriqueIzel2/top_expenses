c079f2ed9b1de5cb2e839c6c9ab0b5e3784a1bca
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-30 16:30:50 -0400

    feat: add the component from main.dart

--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'package:top_expenses/models/transaction.dart';
 
@@ -9,6 +10,54 @@ class TransactionsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Tensacriton");
+    return Column(
+      children: transactions.map((transaction) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  ),
+                ),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  "R\$ ${transaction.value.toStringAsFixed(2)}",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    transaction.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    DateFormat('d MMM y').format(transaction.date),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
   }
 }
