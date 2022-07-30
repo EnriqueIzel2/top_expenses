b62022caf850ac1cbe9a903b30d1485cec0a58f0
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-30 16:34:29 -0400

    refactor: refactor the Column widget to TransactionsList widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:top_expenses/components/transactions_list.dart';
 
 import 'package:top_expenses/models/transaction.dart';
 
@@ -54,55 +54,7 @@ class HomeScreen extends StatelessWidget {
                 child: Text("Gráfico"),
               ),
             ),
-            Column(
-              children: _transactions.map((transaction) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          "R\$ ${transaction.value.toStringAsFixed(2)}",
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transaction.title,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(transaction.date),
-                            style: const TextStyle(
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              }).toList(),
-            ),
+            TransactionsList(transactions: _transactions),
             Card(
               elevation: 5,
               child: Padding(
