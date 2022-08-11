e867faff5bfebc3c1a62d1eecf17b7e0ae8a56a3
EnriqueIzel2 <eleaoizel@gmail.com>
2022-08-11 10:24:26 -0400

    feat: add function and TODO(i'm sorry)

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -33,10 +33,12 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    groupedTransactions;
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
       child: Row(
+        // TODO implementar o widget que mostra o percentual
         children: [],
       ),
     );
