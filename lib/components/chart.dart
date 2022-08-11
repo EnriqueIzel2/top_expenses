383f20692b8796bf55e9fad216ac259b9ac91eae
EnriqueIzel2 <eleaoizel@gmail.com>
2022-08-11 08:59:57 -0400

    feat: create the chart widget

---
+++ lib/components/chart.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'package:top_expenses/models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  const Chart({Key? key, required this.recentTransactions}) : super(key: key);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      final dayFirstLetter = DateFormat.E().format(weekDay)[0];
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        bool isSameDay = recentTransactions[i].date.day == weekDay.day;
+        bool isSameMonth = recentTransactions[i].date.month == weekDay.month;
+        bool isSameYear = recentTransactions[i].date.year == weekDay.year;
+
+        if (isSameDay && isSameMonth && isSameYear) {
+          totalSum += recentTransactions[i].value;
+        }
+      }
+
+      return {'day': dayFirstLetter, 'value': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
