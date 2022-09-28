217630c4ba76afed6fafe212730e78fa59190ba7
EnriqueIzel2 <eleaoizel@gmail.com>
2022-09-27 20:43:24 -0400

    feat: remove Container widget

--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -13,65 +13,62 @@ class TransactionsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.64,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  "Nenhuma transação cadastrada",
-                  style: Theme.of(context).textTheme.headline6,
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              Text(
+                "Nenhuma transação cadastrada",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  "assets/images/waiting.png",
+                  fit: BoxFit.cover,
                 ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    "assets/images/waiting.png",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: ((context, index) {
-                final transaction = transactions[index];
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: ((context, index) {
+              final transaction = transactions[index];
 
-                return Card(
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text(
-                            "R\$${transaction.value}",
-                            style: const TextStyle(color: Colors.white),
-                          ),
+              return Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text(
+                          "R\$${transaction.value}",
+                          style: const TextStyle(color: Colors.white),
                         ),
                       ),
                     ),
-                    title: Text(
-                      transaction.title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat('d MMM y', "pt_BR").format(transaction.date),
-                    ),
-                    trailing: IconButton(
-                      onPressed: () => onRemove(transaction.id),
-                      icon: const Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                    ),
                   ),
-                );
-              }),
-            ),
-    );
+                  title: Text(
+                    transaction.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat('d MMM y', "pt_BR").format(transaction.date),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () => onRemove(transaction.id),
+                    icon: const Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                  ),
+                ),
+              );
+            }),
+          );
   }
 }
